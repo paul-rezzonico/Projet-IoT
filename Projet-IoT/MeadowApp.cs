55d7881ae7e181e007ca8b48d897f7d89252155a
paul-rezzonico <paul.rezzo@gmail.com>
2026-03-24 13:55:27 +0100

    feat: implement MQTT client for Azure IoT Hub data transmission

--- Projet-IoT/MeadowApp.cs
+++ Projet-IoT/MeadowApp.cs
@@ -3,12 +3,87 @@ using Meadow.Devices;
 using Meadow.Foundation.Sensors.Atmospheric;
 using System;
 using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Formatter;
+using MQTTnet.Protocol;
+using System.Text;
 
 namespace MeadowApp
 {
     public class MeadowApp : App<F7FeatherV2>
     {
         Bmp280? bmp;
+        
+        public bool sendDataToAzure(double temperature, double pressure)
+        {
+            try
+            {
+                // Azure IoT Hub Configuration
+                string hostname = "meadow-iot-hub.azure-devices.net";
+                string deviceId = "meadow-device";
+                string sasToken = "SharedAccessSignature sr=meadow-iot-hub.azure-devices.net%2Fdevices%2Fmeadow-device&sig=NNGAYMWdTucxAe%2BSysHUMMl%2BWNNd3XJ3d2uqS7o2PXo%3D&se=1774344713";
+                
+                // MQTT Configuration
+                string clientId = $"{hostname}/{deviceId}/?api-version=2021-04-12";
+                string username = $"{hostname}/{deviceId}/?api-version=2021-04-12";
+                string topic = $"devices/{deviceId}/messages/events/";
+                
+                // Create MQTT client
+                var client = new MqttFactory().CreateMqttClient();
+                
+                var options = new MqttClientOptionsBuilder()
+                    .WithTcpServer(hostname, 8883)
+                    .WithTlsOptions(new MqttClientTlsOptionsBuilder()
+                        .WithAllowUntrustedCertificates()
+                        .Build())
+                    .WithCredentials(username, sasToken)
+                    .WithClientId(clientId)
+                    .Build();
+                
+                // Connect to Azure IoT Hub
+                var connectResult = client.ConnectAsync(options).Result;
+                
+                if (connectResult.ResultCode != MqttClientConnectResultCode.Success)
+                {
+                    Console.WriteLine($"Failed to connect to Azure IoT Hub: {connectResult.ResultCode}");
+                    return false;
+                }
+                
+                Console.WriteLine("Successfully connected to Azure IoT Hub");
+                
+                // Prepare JSON payload using string formatting (compatible with netstandard2.1)
+                string jsonPayload = $"{{\"temperature\":{temperature},\"pressure\":{pressure},\"timestamp\":\"{DateTime.UtcNow:O}\"}}";
+                
+                var message = new MqttApplicationMessageBuilder()
+                    .WithTopic(topic)
+                    .WithPayload(jsonPayload)
+                    .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+                    .Build();
+                
+                // Send message
+                var publishResult = client.PublishAsync(message).Result;
+                
+                if (publishResult.IsSuccess)
+                {
+                    Console.WriteLine($"Message sent successfully: {jsonPayload}");
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to send message: {publishResult.ReasonCode}");
+                }
+                
+                // Disconnect
+                client.DisconnectAsync().Wait();
+                
+                return publishResult.IsSuccess;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending data to Azure: {ex.Message}");
+                return false;
+            }
+        }
 
         public override async Task Initialize()
         {
